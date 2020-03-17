18b2edd4811a27c90fef1b4e3ea0514386c43af2
Jieun Kang <jieunkang101@gmail.com>
2020-03-17 10:08:48 -0700

    add routes for Animals controller
    Co-authored-by: Jieun Kang <jieunkang101@gmail.com>
    > Co-authore-by: Matt Taylor <taylor.matt@protonmail.com>
    > Co-authored-by: Fatma Dogan <fceyda@gmail.com>

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalShelter.Controllers
+{
+  public class AnimalsController : Controller
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Animal> model = _db.Animals.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
