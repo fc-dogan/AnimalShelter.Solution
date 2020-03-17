6afe19036bb525e9cd79f811c6ad22f54f85a7d5
Jieun Kang <jieunkang101@gmail.com>
2020-03-17 16:20:01 -0700

    add an enumeration type to add default types of animal
    Co-authored-by: Matt Taylor <taylor.matt@protonmailcom>
    Co-auatored-by: Jieun Kang <jieunkang101@gmail.com>
    Co-authored-by: Fatma Dogan <fceyda@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -5,10 +5,18 @@ namespace AnimalShelter.Models
   public class Animal
   {
     public int AnimalId { get; set; }
-    public string Type { get; set; }
+    public Type AnimalType { get; set; }
     public string Name  { get; set; }
     public string Gender  { get; set; }
     public DateTime Date  { get; set; }
     public string Breed  { get; set; }
+  }
+
+  public enum Type
+  {
+    Dog,
+    Cat,
+    Rabbit,
+    Koala
   }
 }
